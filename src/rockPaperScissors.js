e14ad994b0cf2c697a237026f9e1ef496de11c3c
Brittany Hartmire <bkhartmire@gmail.com>
2019-06-04 20:10:15 +0900

    rockPaperScissors

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -17,6 +17,20 @@
  *   rockPaperScissors(4); // => [['rock', 'rock', 'rock', 'rock'], etc...]
  */
 
-const rockPaperScissors = () => {
+const rockPaperScissors = (rounds = 3) => {
   // Your code here
+  const results = [];
+  const weapons = ['rock', 'paper', 'scissors'];
+  
+  function recurse(roundsLeft, played) {
+    if(roundsLeft === 0) {
+      results.push(played);
+      return;
+    }
+    for(let i = 0; i < weapons.length; i++) {
+      recurse(roundsLeft-1, played.concat(weapons[i]));
+    }
+  }
+  recurse(rounds, []);
+  return results;
 };
