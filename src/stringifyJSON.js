c5471c1abb839869a0f9008d5f0c24267099502a
Brittany Hartmire <bkhartmire@gmail.com>
2019-06-04 23:39:29 +0900

    finish stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,6 +6,30 @@ What does the output for stringifyJSON look like? Play around with JSON.stringif
 
 */
 
-const stringifyJSON = () => {
+const stringifyJSON = (value) => {
   // YOUR CODE HERE
+  if (value === undefined) {
+    return undefined;
+  } else if (value === null) {
+    return 'null';
+  } else if (typeof value === "string") {
+    return '\"' + value + '\"';
+  } else if (typeof value === "number" || typeof value === "boolean") {
+    //surprisingly works as desired for booleans too
+    return value.toString(10);
+  } else if (Array.isArray(value)) {
+    return '[' + value.reduce((result, item) => {
+        return [...result, stringifyJSON(item)]
+    }, []).join(',') + ']'
+  } else if (typeof value === "object") {
+    let stringafiableObject = {};
+    Object.keys(value).forEach((key) => {
+      if (typeof key !== "function" && key !== undefined && value[key] !== undefined && typeof value[key] !== "function") {
+        stringafiableObject[key] = value[key];
+      }
+    })
+    return '{' + Object.keys(stringafiableObject).reduce((result, key) => {
+      return [...result, stringifyJSON(key) + ':' + stringifyJSON(stringafiableObject[key])]
+    }, []).join(',') + '}';
+  }
 };
