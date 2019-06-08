bd652a03b7e5acce3cf525727f3c57992c356067
Stefano Demichelis <steppashr@gmail.com>
2019-06-08 17:56:48 +0900

    Recursion, stringifyJSON, 1-12 working fine, tackling objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,36 +1,77 @@
-/* exported stringifyJSON */
+  /* exported stringifyJSON */
 
-/*
+  /*
 
-What does the output for stringifyJSON look like? Play around with JSON.stringify to find out!
-  let l = new Date(Date.now()).toString()
+  What does the output for stringifyJSON look like? Play around with JSON.stringify to find out!
+    let l = new Date(Date.now()).toString()
 
-*/
-  
-const stringifyJSON = (enne) => {
+  */
+    
+  const stringifyJSON = (value) => {
+  console.log(value)
+    let result = [];
 
-  let open = "";
-  let close = "";
-  let result = [];
-
-  function recursive(core) {
-    if (typeof core === "number" || typeof core === "boolean" || core === null) {
-      result.push(`` + core + ``);
-    } else if (typeof core === "string") {
-      result.push(`"` + core + `"`);
-    } else if (Array.isArray(core)) {
-      open += "[";
-      close += "]"
-      if (core.length === 0) {
-        return;
-      }
-      for (let key of core) {
-        recursive(key)
+    function recursive(core) {
+      if (typeof core === "number" || typeof core === "boolean" || core === null) {
+        result.push(recursDoubleQuotes(core));
+      } else if (typeof core === "string") {
+        result.push(recursSingleQuotes(core));
+      } else if (Array.isArray(core)) {
+        recursArray(core);
+      } else if (typeof core === "object") {
+        recursObj(core);
       }
     }
-  }
 
-  
-  recursive(enne);
-  return open + result.join(',') + close
-}
+    function recursObj(obj) {
+      if(Object.keys(obj).length === 0) {
+        result.push("{}")
+      } else {
+        return "ciao"
+      }
+    }
+
+    function recursDoubleQuotes(double) {
+      return `` + double + ``;
+    }
+
+    function recursSingleQuotes(single) {
+      return `"` + single + `"`
+    }
+
+    function recursArray(arr) {
+      if (arr.length===0) {
+        result.push("[]");
+      };
+      for (let i of arr) {
+        if (i === arr[0]) {
+        result.push("[");
+        }
+        if (Array.isArray(i)) {
+          recursArray(i);
+          if (i !== arr[arr.length-1]) {
+            result.push(",")
+          }
+        } else {
+          if (i === arr[arr.length-1]) {
+            if (typeof i === "string") {
+              result.push(recursSingleQuotes(i));
+            } else {
+              result.push(i)
+            }
+          } else {
+            result.push(i+",");
+          }
+        }
+        if (i === arr[arr.length-1]) {
+          result.push("]");
+        }
+      }
+    }
+      
+
+
+
+  recursive(value);
+  return result.join('');
+  }
