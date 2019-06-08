ee0beb235761d458bb0d76c32333652b4e0f1dab
Stefano Demichelis <steppashr@gmail.com>
2019-06-08 14:58:11 +0900

    Recursion, stringifyJSON tests 1-10 pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,9 +3,37 @@
 /*
 
 What does the output for stringifyJSON look like? Play around with JSON.stringify to find out!
+  let l = new Date(Date.now()).toString()
 
 */
+  
+const stringifyJSON = (enne) => {
+  console.log(enne)
+  console.log(JSON.stringify(enne))
+  // return JSON.stringify(enne)
 
-const stringifyJSON = () => {
-  // YOUR CODE HERE
-};
+  let open = "";
+  let close = "";
+  let result = [];
+
+  function recursive(core) {
+    if (typeof core === "number" || typeof core === "boolean" || core === null) {
+      result.push(`` + core + ``);
+    } else if (typeof core === "string") {
+      result.push(`"` + core + `"`);
+    } else if (Array.isArray(core)) {
+      open += "[";
+      close += "]"
+      if (core.length === 0) {
+        return;
+      }
+      for (let key of core) {
+        recursive(key)
+      }
+    }
+  }
+
+  
+  recursive(enne);
+  return open + result.join(',') + close
+}
